1cd3ae3614551958748b06d22113b088c7df2755
chinacheng <chinachengliwen@gmail.com>
2012-10-14 15:14:37 +0800

    select a better event, and change it

--- chrome/background.js
+++ chrome/background.js
@@ -1,15 +1,33 @@
 ﻿//chrome.tabs.onCreated.addListener(function(tab) {
-//  alert(1);
+//  alert('this is a event that will fired when a tab is created');
 //});
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if(changeInfo.status == 'loading'){
-    var url = document.createElement('a');
-    url.href = (tab.url);
-    if(url.protocol == 'http:' && /google/.test(url.hostname)){
-      new_url = url.href.replace(/^http/,"https");
-      chrome.tabs.update(tabId, {url:new_url});
-    }else{
-      return;
-    };  
-  }
-});
+
+
+// This is a event that will be fired when the tab is updated
+//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//  if(changeInfo.status == 'loading'){
+//    var url = document.createElement('a');
+//    url.href = (tab.url);
+//    if(url.protocol == 'http:' && /google/.test(url.hostname)){
+//      new_url = url.href.replace(/^http/,"https");
+//      chrome.tabs.update(tabId, {url:new_url});
+//    }else{
+//      return;
+//    };  
+//  }
+//});
+
+// The onBeforeRequest event will be better, filter and change the matched request url
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+		return{redirectUrl : details.url.replace(/^http/,"https")};
+  },
+  {
+    urls :[
+					  "http://www.google.com.hk/*",
+						"http://www.google.com/*"
+					]
+  },
+  ["blocking"]
+);
+
