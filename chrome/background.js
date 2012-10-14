e97321dd290a67581ede7f32af5f44c441abb615
chinacheng <chinachengliwen@gmail.com>
2012-10-14 13:02:24 +0800

    commit the chrome plugin

---
+++ chrome/background.js
@@ -0,0 +1,15 @@
+﻿//chrome.tabs.onCreated.addListener(function(tab) {
+//  alert(1);
+//});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if(changeInfo.status == 'loading'){
+    var url = document.createElement('a');
+    url.href = (tab.url);
+    if(url.protocol == 'http:' && /google/.test(url.hostname)){
+      new_url = url.href.replace(/^http/,"https");
+      chrome.tabs.update(tabId, {url:new_url});
+    }else{
+      return;
+    };  
+  }
+});
