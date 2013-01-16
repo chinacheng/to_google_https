97501c266dedcaa90cad4b8a23e0f9a547104563
chinacheng <chinachengliwen@gmail.com>
2013-01-16 17:48:01 +0800

    try to modify the url  of the current request, but not success

--- firefox/extensions/to_google_https/chrome/content/javascripts/overlay.js
+++ firefox/extensions/to_google_https/chrome/content/javascripts/overlay.js
@@ -1,15 +1,48 @@
+var TOPIC_MODIFY_REQUEST = "http-on-modify-request";
+//var TOPIC_MODIFY_REQUEST = "http-on-opening-request";
+var RE_URL_TO_MODIFY = /^http:\/\/www.google.com*/;
+// 定义观察者
 var httpRequestObserver =
 {
-  observe: function(subject, topic, data) 
-  {
-    if (/google/.test(topic)) {
-	  alert(topic)
-      var httpChannel = subject.QueryInterface(Components.interfaces.nsIHttpChannel);
-      httpChannel.setRequestHeader("X-Hello", "World", false);
-    }
+  observe : function(aSubject, aTopic, aData) {
+  if (TOPIC_MODIFY_REQUEST == aTopic) {
+    let url;
+	
+    var httpChannel = aSubject.QueryInterface(Components.interfaces.nsIHttpChannel);
+    url = aSubject.URI.spec;
+ 
+    if (RE_URL_TO_MODIFY.test(url)) { // RE_URL_TO_MODIFY is a regular expression.
+	  url = url.replace(/^http/,"https");
+	  httpChannel.setRequestHeader('Location', url, false);
+	  
+	  //var ios = Components.classes["@mozilla.org/network/io-service;1"].getService(Components.interfaces.nsIIOService);
+	  //var uri = ios.newURI(url, null, null);
+	  //var ch = ios.newChannelFromURI(uri);
+	  
+	  
+	  //var ios = Components.classes["@mozilla.org/network/io-service;1"]
+      //              .getService(Components.interfaces.nsIIOService);
+	  //var ch = ios.newChannel(url, null, null);
+	  
+	  //var channel = ioService.newChannelFromURI(uri);
+	  //var listener = new StreamListener(callbackFunc);
+	  //channel.notificationCallbacks = listener;
+	  //channel.asyncOpen(listener, null);
+	  
+	  //aSubject.setRequestHeader("Referer", url, false);
+	  //window.location.href = url;
+	  //aSubject.setRequestHeader('Location', url, true);
+    } 
+	//else if (RE_URL_TO_CANCEL.test(url)) { // RE_URL_TO_CANCEL is a regular expression.
+    //  aSubject.cancel(Components.results.NS_BINDING_ABORTED);
+    //}
+   }
   }
 };
 
-var observerService = Components.classes["@mozilla.org/observer-service;1"]
-                                .getService(Components.interfaces.nsIObserverService);
-observerService.addObserver(httpRequestObserver, window.location.href, false);
+// 观察者事件绑定
+//var observerService = Components.classes["@mozilla.org/observer-service;1"]
+//                               .getService(Components.interfaces.nsIObserverService);
+//observerService.addObserver(httpRequestObserver, TOPIC_MODIFY_REQUEST, false);
+
+
